5667213e84826a13a312b6f4ea294a989cafa69a
Hnng2011 <luphihung111@gmail.com>
2025-01-27 13:16:36 +0700

    Update linktree

--- src/main.tsx
+++ src/main.tsx
@@ -6,22 +6,57 @@ import './index.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import Logo from '@assets/Logo.png';
 import LogoName from '@/assets/logo-name.jpg';
-import { SparklesText } from './component/UI/sparkles';
-import Bg from '@assets/bg.png';
 
 const TempLanding = () => {
   return (
-    <div className="h-full w-full fixed bg-[#FFFCF1]">
-      <div className="relative mt-4 ml-4">
+    <div className="w-full fixed bg-[#FFFCF1] flex flex-col justify-items items-center h-screen">
+      <div className="relative mt-4 ml-4 w-60">
         <img src={Logo} className="w-20 h-20 relative z-10" />
         <img
           src={LogoName}
           className="absolute translate-x-2 -translate-y-20 w-80 flex-shrink-0 z-0"
         />
       </div>
-      <div className="mx-auto w-fit mt-[40%] lg:mt-[10%] font-artistDAO">
-        <SparklesText className="text-7xl lg:text-9xl" text={'Coming Soon'} />
-        <img className="w-[50%] md:w-[20%] fixed bottom-0 right-0 " src={Bg} />
+
+      <div className="mx-auto w-fit font-artistDAO mt-6">
+        <a
+          href="https://discord.gg/ngMkRJrR"
+          className="flex items-center gap-4 px-4 py-2 border-2 rounded-3xl hover:border-black duration-300">
+          <div className="text-2xl font-bold inline-flex gap-4 items-center">
+            {' '}
+            <img
+              src="https://img.icons8.com/?size=100&id=25627&format=png&color=000000"
+              className="w-6 h-6"
+            />{' '}
+            Discord:
+          </div>
+          <div className="font-bold">https://discord.gg/ngMkRJrR</div>
+        </a>
+        <a
+          href="https://t.me/Artdaodotfun"
+          className="flex items-center gap-4 px-4 py-2 border-2 rounded-3xl hover:border-black duration-300 mt-4">
+          <div className="text-2xl font-bold inline-flex gap-4 items-center">
+            <img
+              src="https://img.icons8.com/?size=100&id=TCnKnYZFoOzM&format=png&color=000000"
+              className="w-6 h-6"
+            />{' '}
+            Telegram:
+          </div>
+          <div className="font-bold">https://t.me/Artdaodotfun</div>
+        </a>
+
+        <a
+          href="https://x.com/Artdaodotfun"
+          className="flex items-center gap-4 px-4 py-2 border-2 rounded-3xl hover:border-black duration-300 mt-4">
+          <div className="text-2xl font-bold inline-flex gap-4 items-center">
+            <img
+              src="https://img.icons8.com/?size=100&id=fJp7hepMryiw&format=png&color=000000"
+              className="w-6 h-6"
+            />{' '}
+            X:
+          </div>
+          <div className="font-bold">https://x.com/Artdaodotfun</div>
+        </a>
       </div>
     </div>
   );
